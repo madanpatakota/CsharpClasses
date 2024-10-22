77e08d481f3e9186425a4d6d420306b22c223e75
Madn <madan.patakota@gmail.com>
2024-10-22 20:25:33 +0530

    files

---
+++ CsharpClasses/1.Class_Basics.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+// class have the fields , properties , methods and constructor
+
+class File
+{
+    string fileName; // field member
+    string fileLocation;
+    string fileSize;
+    string createdDate;
+
+    //Main Member
+    static void Main()
+    {
+
+        //string str = "Madan";
+        //string srr1 = "Madan";
+
+        //new File().ShowMessage();
+        string str = "asdfasdfas";
+
+        //Array of File
+        //File[] files = new File[]
+        //{
+
+        //}
+
+        File file = new File();
+        file.ShowMessage("Resume.docx", @"D:/Eductation"); // string
+
+        //---------------------------------------------------------------
+
+        File file1 = new File();
+        file1.fileName = "HDFCCreditcardStatement.pdf";
+        file1.createdDate = "May-10-2024";
+        //ShowPDFFileHistory("HDFCCreditcardStatement.pdf", "May-10-2024";);
+        ShowPDFFileHistory(file1.fileName, file1.createdDate);
+
+        File file2 = new File();
+        file2.fileName = "VisualStudioCodeInstallizationdocument.docx";
+        file2.fileSize = "10kb";
+
+        //ShowWordFileSize("VisualStudioCodeInstallizationdocument.docx", "10kb");
+        ShowWordFileSize(file2.fileName, file2.fileSize);
+
+
+        //string fileName; // field member
+        //string fileLocation;
+        //string fileSize;
+        //string createdDate;
+
+        File file3 = new File();
+        file3.fileName = "Sasi_FullStatckDeveloperResume.docx";
+        file3.createdDate = "Oct-10-2024";
+        file3.fileLocation = @"D:/eDucation";
+        file3.fileSize = "10kb";
+
+        Console.WriteLine("Test File3" + file3.fileName);
+        Console.WriteLine("Test File3" + file3.createdDate);
+        Console.WriteLine("Test File3" + file3.fileLocation);
+        Console.WriteLine("Test File3" + file3.fileSize);
+
+
+        File file4 = new File()
+        {
+            fileName = "Sasi_FullStatckDeveloperResume.docx",
+            createdDate = "Oct-10-2024",
+            fileLocation = @"D:/eDucation",
+            fileSize = "10kb"
+        };
+
+
+
+
+        Console.ReadLine();
+    }
+
+    //non-static method
+    void ShowMessage(string name, string loc) // method member
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(loc);
+    }
+
+    static void ShowPDFFileHistory(string name, string createdDate)
+    {
+        Console.WriteLine($"Hello , File name is {name} and creareddate {createdDate}");
+    }
+
+    static void ShowWordFileSize(string name, string filesize)
+    {
+        Console.WriteLine($"Hello, File name is {name} and filelocation is {filesize}");
+    }
+
+
+}
+
---
+++ CsharpClasses/2.Classes_Advanced.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class ExcelProgram
+{
+    static void Main()
+    {
+        string[] fileNames =
+        {
+            "Excelfile1.xlsx" , "Excelfile2.xlsx" , "Excelfile3.xlsx"
+        };
+
+        // i want to call ShowExcelFileNamesLength method from ExcelFilesSystem;
+        ExcelFilesSystem excelFilesSystem = new ExcelFilesSystem();
+        excelFilesSystem.ShowExcelFilesLength(fileNames);
+        // github // public
+        //accessmodifier public
+
+        Console.ReadLine();
+    }
+}
+
+
+class ExcelFile
+{
+    string fileName; // field member
+    string fileLocation;
+    string fileSize;
+    string createdDate;
+}
+
+
+class ExcelFilesSystem
+{
+    public void ShowExcelFilesLength(string[] fileNames)
+    {
+        Console.WriteLine(fileNames.Length + " Files");  // 2 files
+    }
+
+}
+
